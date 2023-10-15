09cc970ed90f8b39a6f7201cdb9932efbcc02f11
Grisha G <grachssuper2@gmail.com>
2023-10-15 20:32:40 +0300

    hw1 commit

--- src/main/java/edu/hw1/Task5.java
+++ src/main/java/edu/hw1/Task5.java
@@ -1,7 +1,5 @@
 package edu.hw1;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 
 public class Task5 {
     //private final static Logger LOGGER = LogManager.getLogger();
